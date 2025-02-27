31f33c1189fb4af612cdb2acfcbcd66ae89f5e11
suhana99 <suhanakafle@gmail.com>
2025-02-27 22:43:34 +0545

    checkout

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Cart, Product
+from .models import Cart, Product, Order
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,3 +12,22 @@ class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
         fields = ['id', 'product','quantity']
+
+class OrderSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
+    
+    class Meta:
+        model = Order
+        fields = [
+            'id', 'user', 'products', 'quantity', 'total_price', 'delivery_status',
+            'payment_method', 'payment_status', 'contact_no', 'address', 'created_date'
+        ]
+        read_only_fields = ['id', 'total_price', 'delivery_status', 'payment_status', 'created_date']
+
+    def create(self, validated_data):
+        products_data = validated_data.pop('products')
+        order = Order.objects.create(**validated_data)
+        
+        for product in products_data:
+            order.products.add(product)  # Assuming a ManyToMany relationship
+        return order
