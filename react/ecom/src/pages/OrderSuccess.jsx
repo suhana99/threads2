c8304db5ad465f13a6a0241c47c7d18c842bf3ce
suhana99 <suhanakafle@gmail.com>
2025-03-09 20:17:58 +0545

    order

--- react/ecom/src/pages/OrderSuccess.jsx
+++ react/ecom/src/pages/OrderSuccess.jsx
@@ -4,12 +4,10 @@ import { useLocation } from "react-router-dom";
 
 const OrderSuccess = () => {
   const location = useLocation();
-  const orderId = location.state?.orderId;
 
   return (
     <div>
       <h2 style={{padding:'25px 0px 5px 120px'}}>Order Placed Successfully!</h2>
-      {/* <p>Your order ID is: {orderId}</p> */}
     </div>
   );
 };
